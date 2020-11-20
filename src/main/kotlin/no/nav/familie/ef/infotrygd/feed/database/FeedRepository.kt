f890fc2cf6ad012cdf3441a3144f37bbe76adc21
Johan Blomgren <johan.blomgren@nav.no>
2020-11-20 16:14:23 +0100

    Ryddet opp og lagt til sonar

--- src/main/kotlin/no/nav/familie/ef/infotrygd/feed/database/FeedRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/infotrygd/feed/database/FeedRepository.kt
@@ -5,8 +5,6 @@ import org.springframework.data.repository.CrudRepository
 
 interface FeedRepository : CrudRepository<Feed, Long> {
 
-    /*@Query(value = "SELECT f FROM Feed f WHERE f.sekvensId > :sistLesteSekvensId AND f.duplikat = false ORDER BY f.sekvensId asc")
-    fun finnMeldingerMedSekvensIdStørreEnn(pageable: Pageable, sistLesteSekvensId: Long): List<Feed>*/
     fun findBySekvensIdGreaterThanOrderBySekvensIdAsc(pageable: Pageable, sekvensId: Long): List<Feed>
 
 }
