33fb89b8db254db2a8043d9b908d61997cdc6968
Johan Blomgren <johan.blomgren@nav.no>
2020-11-20 16:06:53 +0100

    Må kanskje ha med validator

--- src/main/kotlin/no/nav/familie/ef/infotrygd/feed/config/FlywayConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/infotrygd/feed/config/FlywayConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnProperty("spring.flyway.enabled")
+@ConditionalOnProperty("spring.cloud.vault.database.enabled")
 class FlywayConfiguration {
 
     @Bean
