c772eaaaf54dedf87a0d097006823482bb19735d
Johan Blomgren <johan.blomgren@nav.no>
2020-11-20 15:54:34 +0100

    Setter namespace

--- src/test/kotlin/no/nav/familie/ef/infotrygd/feed/config/SecurityTestConfig.kt
+++ src/test/kotlin/no/nav/familie/ef/infotrygd/feed/config/SecurityTestConfig.kt
@@ -1,18 +1,9 @@
 package no.nav.familie.ef.infotrygd.feed.config
 
 import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.Primary
 
 @Configuration
 @Import(TokenGeneratorConfiguration::class)
-class SecurityTestConfig{
-
-    @Bean
-    @Primary
-    @Qualifier("oidcTokenValidationFilterRegistrationBean")
-    fun abc() = "s"
-}
+class SecurityTestConfig
